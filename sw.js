e98334643fa81804f21e9d6e3df967ceeba2ad72
KCMomentum <kilian@momentumdigital.nl>
2026-05-30 17:43:41 +0200

    Madagascar 2026 offline PWA: interactive map + manifest + service worker + icons

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE = 'mad-2026-05-30';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png',
+  'icon-180.png',
+];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((hit) => {
+      if (hit) return hit;
+      return fetch(e.request)
+        .then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE).then((c) => c.put(e.request, copy));
+          return res;
+        })
+        .catch(() => {
+          if (e.request.mode === 'navigate') return caches.match('index.html');
+        });
+    })
+  );
+});
